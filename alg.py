7da93acd3648bd860f88058d83a2acfb037f1265
Ad2Am2 <92311639+Ad2Am2@users.noreply.github.com>
2022-02-26 21:04:37 -0500

    Distance calculator, meter to mile converter
    Added 2 functions:
    1 - Function given by challege in js, translated to python. Output was in meters, added conversion to miles
    
    2 - Function to convert miles to meters. Conversion constant: 0.0006213712

--- alg.py
+++ alg.py
@@ -1,5 +1,9 @@
 #File will contain the algorithm
-print("hello world")
+
+# Import Statements
+import math
+
+
 
 
 # Function to fetch data from the dataset csv file
@@ -7,3 +11,22 @@ def dataFetch():
     dataset = open("./dataset.csv", "r")
     dataset = dataset.readlines()
     #print(dataset) #test for data collection, should print file in console
+
+def dCalc(lat1, lat2, lon1, lon2):
+    R = 6371000 # metres
+    φ1 = lat1 * math.PI/180 # φ, λ in radians
+    φ2 = lat2 * math.PI/180
+    Δφ = (lat2-lat1) * math.PI/180
+    Δλ = (lon2-lon1) * math.PI/180
+
+    a = math.sin(Δφ/2) * math.sin(Δφ/2) + math.cos(φ1) * math.cos(φ2) * math.sin(Δλ/2) * math.sin(Δλ/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    d = R * c # in metres!!! NOT MILES
+
+    return(meter2mile(d))
+
+
+# Function that converts meters to miles
+def meter2mile(meters):
+    return(0.0006213712*meters)
