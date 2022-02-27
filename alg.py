56a38563caa5a5511e9a2f8521522138fbe8265f
Ad2Am2 <92311639+Ad2Am2@users.noreply.github.com>
2022-02-26 20:47:32 -0500

    File reading function
    Created function in alg.py to read the csv file

--- alg.py
+++ alg.py
@@ -1,2 +1,9 @@
 #File will contain the algorithm
 print("hello world")
+
+
+# Function to fetch data from the dataset csv file
+def dataFetch():
+    dataset = open("./dataset.csv", "r")
+    dataset = dataset.readlines()
+    #print(dataset) #test for data collection, should print file in console
