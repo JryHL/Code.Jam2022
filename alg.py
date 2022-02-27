47821f4571891427a98241c185025fbe2c35b9be
jerry <jerry@jerry-20ljs0wh00>
2022-02-26 23:13:25 -0500

    Added helper functions for calculating profit and time

--- alg.py
+++ alg.py
@@ -4,13 +4,13 @@
 import math
 import csv
 
+#Global variables
 global loadDataset
-# Function to fetch data from the dataset csv file
-def dataFetch():
-    global loadDataset 
-    file = open("./dataset.csv", "r")
-    loadDataset = csv.DictReader(file)
-    
+
+# Function that converts meters to miles
+def meterToMile(meters):
+    return(0.0006213712*meters)
+
 
 def dCalc(lat1, lat2, lon1, lon2):
     R = 6371000 # metres
@@ -24,12 +24,28 @@ def dCalc(lat1, lat2, lon1, lon2):
 
     d = R * c # in metres!!! NOT MILES
 
-    return(meter2mile(d))
+    return(meterToMile(d))
 
 
-# Function that converts meters to miles
-def meter2mile(meters):
-    return(0.0006213712*meters)
+#Calculates the profite made from doing a route with a given distance in miles and pickup amount in dollars
+def profitCalc(distance, amount):
+    return (amount - distance * 0.4)
 
-dataFetch()
+#Returns time needed to travel a certain distance, in miles
+def timeCalc(distance):
+    return (distance / 55.0)
 
+# Function to fetch data from the dataset csv file
+def dataFetch():
+    global loadDataset 
+    file = open("./dataset.csv", "r")
+    loadDataset = csv.DictReader(file)
+    
+
+
+
+def main():
+    print("Hello world!")
+
+if __name__ == '__main__':
+    main()
